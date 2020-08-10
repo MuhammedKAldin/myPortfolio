e4849ab765a9804a88503f1a9e1eaaa98d322de2
muhammedk.aldin.github.io <muhammedk.aldin@gmail.com>
2020-08-10 19:59:49 +0200

    first commit

---
+++ script2.js
@@ -0,0 +1,120 @@
+window.onload = function(){
+  //hide the preloader
+  document.querySelector("#overlayer").style.display = "none";
+  document.querySelector(".loader").style.display = "none";
+}
+   
+   function myFunction() {
+             var x = document.getElementById("panel");
+             if(x.style.display === "none")
+            {
+              x.style.display = "block";
+            } 
+              else {
+            x.style.display = "none";
+             }
+
+          }
+
+
+    $("#buzz_Proj").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#Projects_area").offset().top
+    }, 2000);
+    });
+
+    $("#buzz_Proj2").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#Hire_area").offset().top
+    }, 2000);
+    });
+    
+    $("#buzz_Proj3").click(function() {
+    $('html, body').animate({
+         scrollTop: $("#Contact_area").offset().top
+    }, 2000);
+    });
+
+
+  $( "#showDiscordLogo" ).mouseover(function() {
+    $('.showDiscord').css('display','block');
+  });
+
+  
+  $( "#showDiscordLogo" ).mouseleave(function() {
+    $('.showDiscord').css('display','none');
+  });
+    
+    /* ================================== Projects Area ======================================= */
+ 
+    $( "#showMe_1" ).click(function() {
+      $('.preview-place-1').css('display','block');
+    });
+
+    $( "#closure_1" ).click(function() {
+      $('.preview-place-1').css('display','none');
+    });
+ /* -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ */
+
+    $( "#showMe_2" ).click(function() {
+      $('.preview-place-2').css('display','block');
+    });
+
+    $( "#closure_2" ).click(function() {
+      $('.preview-place-2').css('display','none');
+    });
+     /* -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ */
+
+     $( "#showMe_3" ).click(function() {
+      $('.preview-place-3').css('display','block');
+    });
+
+    $( "#closure_3" ).click(function() {
+      $('.preview-place-3').css('display','none');
+    });
+     /* -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ */
+
+     $( "#showMe_4" ).click(function() {
+      $('.preview-place-4').css('display','block');
+    });
+
+    $( "#closure_4" ).click(function() {
+      $('.preview-place-4').css('display','none');
+    });
+     /* -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ */
+
+     $( "#showMe_5" ).click(function() {
+      $('.preview-place-5').css('display','block');
+    });
+
+    $( "#closure_5" ).click(function() {
+      $('.preview-place-5').css('display','none');
+    });
+     /* -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ */
+
+     $( "#showMe_6" ).click(function() {
+      $('.preview-place-6').css('display','block');
+    });
+
+    $( "#closure_6" ).click(function() {
+      $('.preview-place-6').css('display','none');
+    });
+     /* -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ */
+
+     $( "#showMe_7" ).click(function() {
+      $('.preview-place-7').css('display','block');
+    });
+
+    $( "#closure_7" ).click(function() {
+      $('.preview-place-7').css('display','none');
+    });
+     /* -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ */
+
+     $( "#showMe_8" ).click(function() {
+      $('.preview-place-8').css('display','block');
+    });
+
+    $( "#closure_8" ).click(function() {
+      $('.preview-place-8').css('display','none');
+    });
+    
